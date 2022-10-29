b84406c6c7e284f5b26395b8987dffa14a9eb371
Santosh Dahal <exendahal@gmail.com>
2022-10-29 07:34:54 +0545

    Add project files.

---
+++ PlayCoreUpdateTest/IInAppReview.cs
@@ -0,0 +1,13 @@
+﻿namespace PlayCoreUpdateTest
+{
+    /// <summary>
+    /// Interface to provide in-review functionality
+    /// </summary>
+    public interface IInAppReview
+    {
+        /// <summary>
+        /// Function to launch the In-App Review
+        /// </summary>
+        void LaunchReview();
+    }
+}
---
+++ PlayCoreUpdateTest/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PlayCoreUpdateTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Button_Clicked(Object sender, EventArgs e)
+        {
+            var appReviewer = DependencyService.Get<PlayCoreUpdateTest.IInAppReview>();
+            appReviewer.LaunchReview();
+        }
+    }
+}
